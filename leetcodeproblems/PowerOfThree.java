8b736b46ebc6d5ff849e50d9fead5f4dcc31f094
Anuj <80551063+codeanuj@users.noreply.github.com>
2021-04-07 00:15:51 +0530

    Update PowerOfThree.java

--- leetcodeproblems/PowerOfThree.java
+++ leetcodeproblems/PowerOfThree.java
@@ -10,3 +10,7 @@ class PowerOfThree {
         return false;
     }
 }
+//For any M <= N that is a power of 3, M divides N.
+//For any M <= N that is not a power 3, M does not divide N.
+
+//if M divides 1162261467 then it is power of 3 otherwise no
