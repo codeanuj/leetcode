ea2089887b2437dee4fbc9142d2c807c89d7e004
Anuj <80551063+codeanuj@users.noreply.github.com>
2021-07-07 11:23:52 +0530

    Create LinkedListAddTwoNumbers.java

---
+++ leetcodeproblems/LinkedListAddTwoNumbers.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+ //1st approach
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+     
+        int carry=0;
+        int i1;
+        int i2;
+        StringBuilder sb1=new StringBuilder();
+        
+        while(l1!=null && l2!=null){
+            
+            i1=l1.val;
+            i2=l2.val;
+            i1=i1+i2+carry;
+            carry=(i1>9)?1:0;
+            sb1.append(String.valueOf(i1%10));
+            l1=l1.next;
+            l2=l2.next;
+        }
+        
+        while(l1!=null){
+            i1=l1.val+carry;
+            carry=(i1>9)?1:0;
+            sb1.append(String.valueOf(i1%10));
+            l1=l1.next;
+        }
+        while(l2!=null){
+            i2=l2.val+carry;
+            carry=(i2>9)?1:0;
+            sb1.append(String.valueOf(i2%10));
+            l2=l2.next;
+        }
+        
+        if(carry!=0)
+            sb1.append(String.valueOf(carry));
+        
+        ListNode head=null;
+        ListNode prev_node=null;
+        String s= sb1.reverse().toString();
+        
+        for(int i=s.length()-1; i>=0 ;i--){
+            
+            ListNode l = new ListNode(s.charAt(i)-'0',null);
+            if(head==null){
+                head=l;
+            }
+            if(prev_node!=null){
+                prev_node.next=l;
+            }
+            prev_node=l;                        
+        }
+        
+        return head;
+   }
+}
+//2nd approach
+
+
+
+
